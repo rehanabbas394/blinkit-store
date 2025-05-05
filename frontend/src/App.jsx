787518b679db591dc9e8532f32d95b49d7da4bd7
rehanabbas394 <seorehan1122@gmail.com>
2025-05-06 00:52:44 +0500

    feat: intial commit

---
+++ frontend/src/App.jsx
@@ -0,0 +1,12 @@
+import './App.css'
+
+function App() {
+  return (
+  <div className="p-4">
+    <h1 className="text-3xl font-bold text-red-600">Hello Rehan</h1>
+    <p className="mt-2 bg-blue-200 p-2">This is a test paragraph with Tailwind classes</p>
+  </div>
+  )
+}
+
+export default App
